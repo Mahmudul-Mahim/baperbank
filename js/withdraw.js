36e368a76a546f979f50ba3c7f1a953338eac94e
Mr Khan <mahimkhan53@gmail.com>
2023-02-14 21:59:17 +0600

    new updates

--- js/withdraw.js
+++ js/withdraw.js
@@ -3,24 +3,36 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const withdraw = document.getElementById('withdraw-input');
     const newWithdrawAmountString = withdraw.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+    withdraw.value = ''; 
+    if(isNaN(newWithdrawAmount)){
+        alert('Please Provide Valid Number');
+        return;
+    }
     
     // *text output conversion
     const WithdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawString = WithdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawString);
 
-    // *calculation
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    WithdrawTotalElement.innerText = currentWithdrawTotal;
+    withdraw.value = ''; 
 
     // *text output conversion
     const balanceTotalElement = document.getElementById('balance-total');
     const newBalanceString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(newBalanceString);
-
+    
+    if (newWithdrawAmount > previousBalanceTotal ){
+        alert('Not enough balance');
+        return;
+    }
+    // *calculation
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    WithdrawTotalElement.innerText = currentWithdrawTotal;
+    
     // *calculation
     const currentBalanceTotal = previousBalanceTotal - currentWithdrawTotal;
     balanceTotalElement.innerText = currentBalanceTotal;
 
-    withdraw.value = '';  
+     
 })
